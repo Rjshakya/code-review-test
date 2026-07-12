b16b953761899217c29df554aa0cba36a4a9cfaa
Rjshakya <rajkamalshakya07@gmail.com>
2026-07-12 21:49:53 +0530

    add get users

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,48 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+type User = {
+  id: number;
+  name: string;
+};
 
-export default app
+const users: User[] = [
+  {
+    id: 1,
+    name: "raj",
+  },
+  {
+    id: 2,
+    name: "sham",
+  },
+  {
+    id: 3,
+    name: "ben",
+  },
+];
+
+app
+  .get("/", (c) => {
+    return c.text("Hello Hono!");
+  })
+  .get("/users", (c) => {
+    return c.json(users);
+  })
+  .get("/user/:id", (c) => {
+    const { id } = c.req.param();
+    return c.json(users.find((u) => u.id.toString() === id));
+  });
+// .post("/user", async (c) => {
+//   const user = await c.req.json<User>();
+//   users.push(user);
+//   return c.json(user);
+// })
+// .put("/user/:id" , async(c) => {
+//
+//   const {id } = c.req.param()
+//
+//
+// })
+
+export default app;
